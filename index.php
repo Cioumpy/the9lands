ff22aa6bc87c49bdce9581beaff639280efb3780
Cioumpy <cioumpy@gmail.com>
2019-07-19 23:09:11 +0200

    Initial Commit

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
-	include "scripts/config.php";  // Cofiguration file for the Database connection
+/* Login Page == "The Nine Lands" Website. */
+/* ========================================================================== */
+/*
+*/
+
+
+
+	// Includes
+	include "/index-assets/scripts/config.php";  // Configuration file for the Database connection
 ?>
 
 <!DOCTYPE html>
@@ -8,8 +16,11 @@
 
 	<head>
 		<meta charset="utf-8">
-		<title>The Nine Lands Online Tabletop - D&D 3.5</title>
-		<link href="/styles/index.css" rel="stylesheet" type="text/css">
+		<title>The Nine Lands Online Tabletop - d20 System</title>
+		<link href="/assets/styles/index.less" rel="stylesheet/less" type="text/css">
+
+		<!-- Adding Less CSS Precompiler -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/less.js/3.9.0/less.min.js"></script>
 	</head>
 
 	<body>
