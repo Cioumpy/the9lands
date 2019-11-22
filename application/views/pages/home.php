cb87504feb2c08a595ecbe626304c64eba419d7c
Cioumpy <cioumpy@gmail.com>
2019-11-22 02:49:38 +0100

    T9L-2: Written Home page.

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,12 +1,33 @@
-<h2>Welcome HOME!!!</h2>
+<audio autoplay loop>
+	<source src="/media/commons/audio/celtic_music.mp3" type="audio/mpeg">
+	Your browser does not support the audio tag.
+</audio>
 
-<p><a href="<?php echo site_url('pages/logout'); ?>">Logout</a></p>
+<div id="row_top">
 
-<ul><h3>Current User</h3>
-	<li><b><em>Email:</em></b> <?php echo isset($_SESSION['email']) ? $_SESSION['email'] : "<b>NOT SET!</b>"; ?></li>
-	<li><b><em>Name:</em></b> <?php echo isset($_SESSION['first_name']) ? $_SESSION['first_name'] : "<b>NOT SET!</b>"; ?></li>
-	<li><b><em>Surname:</em></b> <?php echo isset($_SESSION['last_name']) ? $_SESSION['last_name'] : "<b>NOT SET!</b>"; ?></li>
-	<li><b><em>Role:</em></b> <?php echo isset($_SESSION['role']) ? $_SESSION['role'] : "<b>NOT SET!</b>" ?></li>
-	<li><b><em>Soft Login:</em></b> <?php echo isset($_SESSION['soft_login']) ? $_SESSION['soft_login'] : "<b>NOT SET!</b>"; ?></li>
-	<li><b><em>Hard Login:</em></b> <?php echo isset($_SESSION['hard_login']) ? $_SESSION['hard_login'] : "<b>NOT SET!</b>"; ?></li>
-</ul>
+	<div id="title_box">
+		<h1></h1>
+		<h2></h2>
+	</div>
+
+	<div id="session_menu">
+		<a type="button" href="<?php echo site_url(); ?>"><span class="glyphicon glyphicon-user"></a>
+		<a type="button" href="<?php echo site_url('pages/logout'); ?>" title="Logout"><span class="glyphicon glyphicon-log-out"></span></a>
+	</div>
+
+</div>
+
+<div id="row_bottom">
+
+	<div id="main_menu">
+
+		<?php foreach ($main_menu as $key => $value): ?>
+			<label class="mainmenu_item"><a href="<?php echo $value; ?>"><?php echo $key; ?></a></label>
+		<?php endforeach; ?>
+
+	</div>
+
+</div>
+
+</body>
+</html>
