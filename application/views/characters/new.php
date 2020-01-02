275fee6967434354cf7996c942becb23a1276432
Cioumpy <cioumpy@gmail.com>
2020-01-02 06:58:16 +0100

    T9L-3A-1: Select Campaign - Written base page with style.

--- application/views/characters/new.php
+++ application/views/characters/new.php
@@ -2,13 +2,45 @@
 
 	<div class="pagetitle">
 		<div class="sideimgbox">
-			<img src="../images/shield.png" height="40px">
+			<img src="/media/characters/logos/dd-logo.png" height="40px">
 		</div>
 		<div class="titlebox">
 			<h2>Select Campaign</h2>
 		</div>
 		<div class="sideimgbox">
-			<img src="../images/shield.png" height="40px">
+			<img src="/media/characters/logos/dd-logo.png" height="40px">
+		</div>
+	</div>
+
+	<div class="mainscreen">
+
+		<div class="subscreen listcol">
+			<div class="listtitle">Campaigns</div>
+		</div>
+
+		<div class="subscreen desccol">
+			<div class="banner">
+				<div class="bannerbox">
+					<span>Custom Character</span>
+				</div>
+			</div>
+			<div class="descbox">
+				<p>You are going to create a standard character of your own choice.</p>
+			</div>
+			<div class="optionbox">
+				<span>
+					<b>Starting Level: </b>
+					<input type="number" min="1" max="20" value="1">
+				</span>
+			</div>
+		</div>
+
+	</div>
+
+	<div class="pagefooter">
+		<div class="pagebuttons">
+			<a type="button" class="mainbtns" href="<?php echo site_url('characters/list'); ?>">Back</a>
+			<button class="mainbtns">Start</button>
 		</div>
 	</div>
 
