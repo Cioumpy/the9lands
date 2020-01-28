9dcd27af53f70a17798e63b5c5fa616b2c088223
Cioumpy <cioumpy@gmail.com>
2020-01-29 00:45:24 +0100

    T9l-3A-2: Minor fixes.

--- application/views/backoffice/accounts.php
+++ application/views/backoffice/accounts.php
@@ -10,39 +10,30 @@
 ?>
 
 
-	<h1><?php echo $subtitle; ?></h1>
+<h1><?php echo $subtitle; ?></h1>
 
-	<div class="title_box">
-		<h2><?php echo $subtitle; ?>' Table:</h2>
-		<button id="create_account_btn"><span class="glyphicon glyphicon-plus"></span> New Account</button>
-	</div>
+<div class="title_box">
+	<h2><?php echo $subtitle; ?>' Table:</h2>
+	<button id="create_account_btn"><span class="glyphicon glyphicon-plus"></span> New Account</button>
+</div>
 
-	<table>
-		<thead>
-			<tr>
-				<th>Email address</th>
-				<th>First Name</th>
-				<th>Last Name</th>
-				<th>Role</th>
-				<th>Actions</th>
-			</tr>
-		</thead>
-		<tbody>
-			<?php foreach ($accounts as $accounts_key => $accounts_item): ?>
-			<tr>
-				<?php foreach ($accounts_item as $item_key => $item_value): ?>
-					<?php if ($item_key != 'password' && $item_key != 'unhashed'): ?>
-						<td class="<?php echo $accounts_key." ".$item_key; ?>"><?php echo $item_value; ?></td>
-					<?php endif; ?>
-				<?php endforeach; ?>
-				<td style="display: flex; justify-content: space-around;">
-					<button class="edit_btn js-update_account_btn" data-row="<?php echo $accounts_key; ?>"><span class="glyphicon glyphicon-pencil"></span></button>
-					<?php echo form_open('backoffice/delete_account'); ?>
-						<?php echo form_hidden('account_to_delete', $accounts_item['email']); ?>
-						<button type="submit" name="submit" class="edit_btn"><span class="glyphicon glyphicon-trash"></span></button>
-					<?php echo form_close(); ?>
-				</td>
-			</tr>
+<table>
+	<thead>
+		<tr>
+			<th>Email address</th>
+			<th>First Name</th>
+			<th>Last Name</th>
+			<th>Role</th>
+			<th>Actions</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($accounts as $accounts_key => $accounts_item): ?>
+		<tr>
+			<?php foreach ($accounts_item as $item_key => $item_value): ?>
+				<?php if ($item_key != 'password' && $item_key != 'unhashed'): ?>
+					<td class="<?php echo $accounts_key." ".$item_key; ?>"><?php echo $item_value; ?></td>
+				<?php endif; ?>
 			<?php endforeach; ?>
 			<td style="display: flex; justify-content: space-around;">
 				<button class="edit_btn js-update_account_btn" data-row="<?php echo $accounts_key; ?>"><span class="glyphicon glyphicon-pencil"></span></button>
